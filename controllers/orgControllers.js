7d44aad4a4af9862702e8702756335803eadc4cf
ciobanoiu andreea elena <andreeaelena@MacBook-Air-ciobanoiu.local>
2025-12-05 20:18:36 +0000

    added orgControllers

---
+++ controllers/orgControllers.js
@@ -0,0 +1,75 @@
+//importam dependentele necesare 
+const Organizator=require('../models/Organizator'); //sa interactionam cu tabela noastra din baza de date
+//biblioteca bcrypt - esentiala pentru securitate, o folosim pentru a cripta(hash) parola inainte de a o salva si pt a o compara la login
+const bcrypt=require('bcrypt'); 
+//JSON web token - odata ce utilizatorul se autentifica trebuie sa gerenam un token('o legitimatie') pe care front end ul il va folosi pentru a accesa rutele securizate 
+const jwt=require('jsonwebtoken');
+//citim cheia folosita de jwt pt a semna si a verifica autenticitatea token urilor
+const SECRET_KEY= process.env.JWT_SECRET;
+
+
+//FUNCTIA REGISTER
+async function register(req, res){
+    const {nume, email, parola}=req.body;
+    
+    try{
+        const existingOrg=await Organizator.findOne({where: {email}});
+        if(existingOrg){
+            return res.status(409).json({message:'email ul este deja folosit'});
+        }
+
+        const hashedPassword=await bcrypt.hash(parola, 10);
+
+        const newOrg=await Organizator.create({
+            nume,
+            email,
+            parola_hash:hashedPassword,
+        });
+
+        res.status(201).json({message:'cont creat cu succes!', organizator:{
+            id:newOrg.id, 
+            nume:newOrg.nume,
+            email:newOrg.email,
+        }});
+        }
+         catch(error){
+         console.error('eroare la inregistrare!', error);
+         res.status(500).json({message:'eroare interna de server'});
+    }
+}
+
+
+//FUNCTIA LOGIN
+async function login(req, res){
+    const {email, parola}=req.body;
+    try{
+        const organizator=await Organizator.findOne({where:{email}});
+        if(!organizator){
+            return res.status(401).json({message:'email sau parola incorecta'});
+        }
+
+        const match=await bcrypt.compare(parola, organizator.parola);
+        if(!match){
+            return res.status(401).json({message:'email sau parola incorecta'});
+        }
+
+        const token=jwt.sign(
+            {
+            id:organizator.id,
+            email:organizator.email
+            },
+            SECRET_KEY,
+            {expiresIn:'1h'}
+    );
+    res.status(200).json({message:'autentificare reusita',token:token,
+        organizator:{
+            id:organizator.id,
+            nume:organizator.nume,
+            email:organizator.email
+        } });
+    }catch(error){
+         console.error('eroare la autentificare!', error);
+         res.status(500).json({message:'eroare interna de server'});
+    }
+}
+module.exports={register,login};
